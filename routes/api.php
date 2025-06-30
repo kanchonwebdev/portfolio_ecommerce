b41c23cec8b2f0380f535ac6678753117632729d
kanchonkumar <kanchonkumar49@gmail.com>
2025-06-30 09:53:19 +0600

    snactum api added

---
+++ routes/api.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Register API
+Route::post('register', function (Request $request) {
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8',
+    ]);
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    return response()->json([
+        'user' => $user,
+        'token' => $user->createToken('auth_token')->plainTextToken
+    ]);
+});
+
+// Login API
+Route::post('login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|string|email',
+        'password' => 'required|string',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return response()->json([
+        'user' => $user,
+        'token' => $user->createToken('auth_token')->plainTextToken
+    ]);
+});
