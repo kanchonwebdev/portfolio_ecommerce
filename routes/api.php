eee5da524c430aab2458095544147a8c4f8ea665
kanchonkumar <kanchonkumar49@gmail.com>
2025-06-30 10:40:30 +0600

    multi auth system added

--- routes/api.php
+++ routes/api.php
@@ -1,52 +1,28 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-// Register API
-Route::post('register', function (Request $request) {
+Route::post('/multi-auth', function (Request $request) {
     $request->validate([
         'name' => 'required|string|max:255',
-        'email' => 'required|string|email|max:255|unique:users',
-        'password' => 'required|string|min:8',
-    ]);
-
-    $user = User::create([
-        'name' => $request->name,
-        'email' => $request->email,
-        'password' => Hash::make($request->password),
-    ]);
-
-    return response()->json([
-        'user' => $user,
-        'token' => $user->createToken('auth_token')->plainTextToken
-    ]);
-});
-
-// Login API
-Route::post('login', function (Request $request) {
-    $request->validate([
-        'email' => 'required|string|email',
+        'email' => 'required|string|email|max:255',
         'password' => 'required|string',
     ]);
 
     $user = User::where('email', $request->email)->first();
 
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
+    if (!$user) {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
         ]);
     }
 
-    return response()->json([
-        'user' => $user,
-        'token' => $user->createToken('auth_token')->plainTextToken
-    ]);
+    Auth::login($user);
+
+    return redirect('https://commerce.scidata-analyst.com/dashboard');
 });
